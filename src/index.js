b49d518c031ac8c25e09f718a14fa5f6c7240440
Cloudymolecule <baires82@gmail.com>
2021-02-18 12:43:57 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const taskDesc = document.getElementById("new-task-description")
+  const taskForm = document.getElementById("create-task-form")
+  
+  taskForm.addEventListener("submit", function(e){
+    e.preventDefault()
+    const ul = document.getElementById("tasks")
+    let li = document.createElement("li")
+    let button = document.createElement("button")
+    button.innerHTML = "X"
+    li.appendChild(document.createTextNode(taskDesc.value))
+    li.appendChild(button)
+    li.setAttribute("id", taskDesc.value) //taskDesc.value
+    ul.appendChild(li)
+    alert(`Added ${taskDesc.value} task`)
+
+    button.addEventListener("click", function(e){
+      li.remove()
+    })
+  })
+
 });
